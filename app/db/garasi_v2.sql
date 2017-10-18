8b4366b6d7e78c158514738a84b564c166b55252
dmadan27 <rarasta27@gmail.com>
2017-10-18 15:13:41 +0700

    database
    => garasi_v2
    => uji coba

--- app/db/garasi_v2.sql
+++ app/db/garasi_v2.sql
@@ -6,7 +6,7 @@
 -- =============================================================
 
 -- tabel admin
-CREATE TABLE m_admin(
+CREATE TABLE admin(
 	username varchar(50) NOT NULL,
 	password text NOT NULL,
 	nama varchar(150),
@@ -21,7 +21,7 @@ CREATE TABLE m_admin(
 -- =============================================================
 
 -- tabel log admin
-CREATE TABLE t_log_admin(
+CREATE TABLE log_admin(
 	id int AUTO_INCREMENT NOT NULL,
 	admin varchar(50),
 	login datetime, -- waktu login
@@ -29,67 +29,58 @@ CREATE TABLE t_log_admin(
 	-- logout datetime, -- waktu logout
 	-- kode_akses text, -- kode akses
 	CONSTRAINT pk_log_admin_id PRIMARY KEY(id),
-	CONSTRAINT fk_log_admin_admin FOREIGN KEY(admin) REFERENCES m_admin(username)
+	CONSTRAINT fk_log_admin_admin FOREIGN KEY(admin) REFERENCES admin(username)
 );
 
 -- =============================================================
 
 -- tabel parkir
-CREATE TABLE m_parkir(
+CREATE TABLE parkir(
 	id int AUTO_INCREMENT NOT NULL,
 	no_parkir char(3) UNIQUE,
 	status char(1), -- 0 : tidak tersedia, 1 : tersedia
-	CONSTRAINT pk_m_parkir_id PRIMARY KEY(id)
+	CONSTRAINT pk_parkir_id PRIMARY KEY(id)
 );
 
 -- =============================================================
 
 -- tabel harga
-CREATE TABLE m_harga(
+CREATE TABLE harga(
 	id int AUTO_INCREMENT NOT NULL,
 	jenis char(1), -- b : bulanan, t : tahunan
 	harga double(10,2) DEFAULT '0.00',
-	CONSTRAINT pk_m_harga_id PRIMARY KEY(id)
+	CONSTRAINT pk_harga_id PRIMARY KEY(id)
 );
 
 -- =============================================================
 
 -- tabel member
-CREATE TABLE m_member(
+CREATE TABLE member(
 	id int AUTO_INCREMENT NOT NULL,
 	no_ktp varchar(20),
 	nama varchar(150),
 	no_telp varchar(20),
 	alamat text,
 	pekerjaan varchar(150),
-	CONSTRAINT pk_m_member_id PRIMARY KEY(id)
+	CONSTRAINT pk_member_id PRIMARY KEY(id)
 );
 
 -- =============================================================
 
 -- tabel nomor polisi kendaraan
-CREATE TABLE m_nopol(
+CREATE TABLE nopol(
 	id int AUTO_INCREMENT NOT NULL,
 	id_member int,
 	nopol varchar(10),
 	foto varchar(25),
-	CONSTRAINT pk_m_nopol_id PRIMARY KEY(id),
-	CONSTRAINT fk_m_nopol_id_member FOREIGN KEY(id_member) REFERENCES m_member(id)
-);
-
--- =============================================================
-
--- tabel saldo total
-CREATE TABLE m_saldo(
-	id int AUTO_INCREMENT NOT NULL,
-	saldo double(12,2) DEFAULT '0.00',
-	CONSTRAINT pk_m_saldo_id PRIMARY KEY(id)
+	CONSTRAINT pk_nopol_id PRIMARY KEY(id),
+	CONSTRAINT fk_nopol_id_member FOREIGN KEY(id_member) REFERENCES member(id)
 );
 
 -- =============================================================
 
 -- tabel sewa
-CREATE TABLE t_sewa(
+CREATE TABLE sewa(
 	id int AUTO_INCREMENT NOT NULL,
 	id_member int,
 	id_parkir int,
@@ -100,28 +91,26 @@ CREATE TABLE t_sewa(
 	jatuh_tempo date,
 	status char(1), -- 0 : kontrak habis, 1 : masih berlaku
 	admin varchar(50),
-	CONSTRAINT pk_t_sewa_id PRIMARY KEY(id),
-	CONSTRAINT fk_t_sewa_id_member FOREIGN KEY(id_member) REFERENCES m_member(id),
-	CONSTRAINT fk_t_sewa_id_nopol FOREIGN KEY(id_nopol) REFERENCES m_nopol(id),
-	CONSTRAINT fk_t_sewa_id_parkir FOREIGN KEY(id_parkir) REFERENCES m_parkir(id),
-	CONSTRAINT fk_t_sewa_admin FOREIGN KEY(admin) REFERENCES m_admin(username)
+	CONSTRAINT pk_sewa_id PRIMARY KEY(id),
+	CONSTRAINT fk_sewa_id_member FOREIGN KEY(id_member) REFERENCES member(id),
+	CONSTRAINT fk_sewa_id_nopol FOREIGN KEY(id_nopol) REFERENCES nopol(id),
+	CONSTRAINT fk_sewa_id_parkir FOREIGN KEY(id_parkir) REFERENCES parkir(id),
+	CONSTRAINT fk_sewa_admin FOREIGN KEY(admin) REFERENCES admin(username)
 );
 
 -- =============================================================
 
 -- tabel keuangan
 -- transaksi selain sewa
-CREATE TABLE t_keuangan(
+CREATE TABLE keuangan(
 	id int AUTO_INCREMENT NOT NULL,
-	-- id_sewa int UNIQUE,
 	tgl date,
 	jenis char(1), -- k : keluar, m : masuk
 	ket text,
 	nominal double(10,2),
 	admin varchar(50),
-	CONSTRAINT pk_t_keuangan_id PRIMARY KEY(id),
-	-- CONSTRAINT fk_t_keuangan_id_sewa FOREIGN KEY(id_sewa) REFERENCES t_sewa(id),
-	CONSTRAINT fk_t_keuangan_admin FOREIGN KEY(admin) REFERENCES m_admin(username)
+	CONSTRAINT pk_keuangan_id PRIMARY KEY(id),
+	CONSTRAINT fk_keuangan_admin FOREIGN KEY(admin) REFERENCES admin(username)
 );
 
 -- =============================================================
@@ -144,29 +133,22 @@ CREATE PROCEDURE tambah_sewa(
 	in admin_param varchar(50)
 )
 BEGIN
-	
-	DECLARE saldo_param double(12,2);
 
-	-- get saldo terakhir
-	SELECT saldo INTO saldo_param FROM m_saldo;
-
-	-- insert t_sewa
-	INSERT INTO t_sewa (
+	-- insert sewa
+	INSERT INTO sewa (
 		id_member, id_parkir, id_nopol, jenis, harga, tgl_sewa, jatuh_tempo, status, admin) 
 	VALUES (
 		id_member_param, id_parkir_param, id_nopol_param, jenis_param, harga_param, tgl_sewa_param, 
 		jatuh_tempo_param, status_param, admin_param);
 
 	-- update status parkir
-	UPDATE m_parkir SET status = "N" WHERE id = id_parkir_param;
+	UPDATE parkir SET status = "0" WHERE id = id_parkir_param;
 
-	-- update saldo
-	UPDATE m_saldo SET saldo = (saldo_param+harga_param);
 END;
 
 -- =============================================================
 
--- procedure edit t_sewa
+-- procedure edit sewa
 -- edit data sewa, jika ada perubahan di harga maka update saldo, 
 -- jika tidak do nothing
 CREATE PROCEDURE edit_sewa(
@@ -183,40 +165,29 @@ BEGIN
 	DECLARE id_parkir_lama int;
 	DECLARE tgl_sewa_lama date;
 	DECLARE harga_lama double(10,2);
-	DECLARE saldo_param double(10,2);
-
+	
 	-- get data parkir sebelumnya
-	SELECT id_parkir INTO id_parkir_lama FROM t_sewa WHERE id = id_param;
+	SELECT id_parkir INTO id_parkir_lama FROM sewa WHERE id = id_param;
 
 	-- get data tgl sebelumnya
-	SELECT tgl_sewa INTO tgl_sewa_lama FROM t_sewa WHERE id = id_param;
+	SELECT tgl_sewa INTO tgl_sewa_lama FROM sewa WHERE id = id_param;
 
 	-- get data harga sebelumnya
-	SELECT harga INTO harga_lama FROM t_sewa WHERE id = id_param;
-
-	-- get data saldo terakhir
-	SELECT saldo INTO saldo_param FROM m_saldo;
+	SELECT harga INTO harga_lama FROM sewa WHERE id = id_param;
 
 	-- cek parkir berubah
 	IF id_parkir_param != id_parkir_lama THEN -- jika ada perubahan
 		-- update parkir lama jd tersedia
-		UPDATE m_parkir SET status = "A" WHERE id = id_parkir_lama;
+		UPDATE parkir SET status = "1" WHERE id = id_parkir_lama;
 		-- update parkir baru jd tidak tersedia
-		UPDATE m_parkir SET status = "N" WHERE id = id_parkir_param;
+		UPDATE parkir SET status = "0" WHERE id = id_parkir_param;
 	END IF;
 
 	-- update t_sewa
-	UPDATE t_sewa SET 
+	UPDATE sewa SET 
 		id_member=id_member_param, id_parkir=id_parkir_param, id_nopol=id_nopol_param, 
 		jenis=jenis_param, harga=harga_param, tgl_sewa=tgl_sewa_param, jatuh_tempo=jatuh_tempo_param 
 	WHERE id=id_param;
-
-	-- cek perubahan harga
-	IF harga_lama != harga_param THEN -- jika ada perubahan
-		-- update saldo
-		-- saldo = saldo terakhir - harga lama + harga baru
-		UPDATE m_saldo SET saldo = ((saldo_param-harga_lama)+harga_param);
-	END IF;
 END;
 
 -- =============================================================
@@ -229,26 +200,15 @@ CREATE PROCEDURE hapus_sewa(
 )
 BEGIN
 	DECLARE id_parkir_param int;
-	DECLARE harga_param double(10,2);
-	DECLARE saldo_param int;
 
 	-- get data id parkir
-	SELECT id_parkir INTO id_parkir_param FROM t_sewa WHERE id = id_param;
-
-	-- get data harga
-	SELECT harga INTO harga_param FROM t_sewa WHERE id = id_param;
-
-	-- get data saldo
-	SELECT saldo INTO saldo_param FROM m_saldo;
+	SELECT id_parkir INTO id_parkir_param FROM sewa WHERE id = id_param;
 
 	-- update parkir
-	UPDATE m_parkir SET status = "A" WHERE id = id_parkir_param;
-
-	-- update saldo
-	UPDATE m_saldo SET saldo = (saldo_param-harga_param);
+	UPDATE parkir SET status = "1" WHERE id = id_parkir_param;
 
 	-- hapus t_sewa
-	DELETE FROM t_sewa WHERE id = id_param;
+	DELETE FROM sewa WHERE id = id_param;
 END;
 
 -- =============================================================
@@ -262,135 +222,22 @@ BEGIN
 	DECLARE id_parkir_param int;
 
 	-- get id parkir
-	SELECT id_parkir INTO id_parkir_param FROM t_sewa where id = id_param;
+	SELECT id_parkir INTO id_parkir_param FROM sewa where id = id_param;
 
 	-- cek status
 	IF status_param = "0" THEN -- jika req. status 0 / kontrak habis
 		-- update status sewa
-		UPDATE t_sewa SET status = "0" WHERE id = id_param;
+		UPDATE sewa SET status = "0" WHERE id = id_param;
 
 		-- update parkir
-		UPDATE m_parkir SET status = "A" WHERE id = id_parkir_param;
+		UPDATE parkir SET status = "1" WHERE id = id_parkir_param;
 	ELSE
 		-- update status sewa
-		UPDATE t_sewa SET status = "1" WHERE id = id_param;
+		UPDATE sewa SET status = "1" WHERE id = id_param;
 
 		-- update parkir
-		UPDATE m_parkir SET status = "N" WHERE id = id_parkir_param;
+		UPDATE parkir SET status = "0" WHERE id = id_parkir_param;
 	END IF;
-END;
-
--- =============================================================
-
--- Procedure tambah keuangan
--- update saldo
-CREATE PROCEDURE tambah_keuangan(
-	in tgl_param date,
-	in jenis_param char(1),
-	in ket_param text,
-	in nominal_param double(10,2),
-	in admin_param varchar(50)
-)
-BEGIN
-	DECLARE saldo_param double(12,2);
-
-	-- get saldo terakhir
-	SELECT saldo INTO saldo_param FROM m_saldo;
-
-	-- insert t_keuangan
-	INSERT INTO t_keuangan (
-		tgl, jenis, ket, nominal, admin) 
-	VALUES (
-		tgl_param, jenis_param, ket_param, nominal_param, admin_param);
-
-	-- cek jenis keuangan
-	IF jenis_param = "M" THEN -- jika uang masuk
-		-- update saldo, saldo = saldo + nominal
-		UPDATE m_saldo SET saldo = (saldo_param+nominal_param);
-	ELSE -- jika uang keluar
-		-- update saldo, saldo = saldo - nominal
-		UPDATE m_saldo SET saldo = (saldo_param-nominal_param);
-	END IF;
-END;
-
--- =============================================================
-
--- procedure edit keuangan
--- update saldo
-CREATE PROCEDURE edit_keuangan(
-	in id_param int,
-	in tgl_param date,
-	in jenis_param char(1),
-	in ket_param text,
-	in nominal_param double(10,2)
-)
-BEGIN
-	DECLARE saldo_param double(12,2);
-	DECLARE jenis_lama char(1);
-	DECLARE nominal_lama double(10,2);
-
-	-- get saldo terakhir
-	SELECT saldo INTO saldo_param FROM m_saldo;
-
-	-- get data jenis
-	SELECT jenis INTO jenis_lama FROM t_keuangan WHERE id = id_param;
-
-	-- get data nominal
-	SELECT nominal INTO nominal_lama FROM t_keuangan WHERE id = id_param;
-
-	-- update t_keuangan
-	UPDATE t_keuangan SET 
-		tgl=tgl_param, jenis=jenis_param, ket=ket_param, nominal=nominal_param
-	WHERE id = id_param;
-
-	-- cek perubahan jenis
-	IF jenis_param != jenis_lama THEN -- jika ada perubahan
-		-- cek masuk / keluar
-		IF jenis_param = "K" THEN -- jika berubah jadi keluar
-			-- update saldo, saldo = (saldo-nominal lama)+nominal baru
-			UPDATE m_saldo SET saldo = ((saldo_param-nominal_lama)+nominal_param);
-		ELSE -- jika berubah jadi masuk
-			-- update saldo, saldo = (saldo+nominal lama)-nominal baru
-			UPDATE m_saldo SET saldo = ((saldo_param+nominal_lama)-nominal_param);
-		END IF;
-	-- cek perubahan nominal
-	ELSE 
-		IF nominal_param != nominal_lama THEN -- jika ada perubahan
-			-- cek masuk / keluar
-			IF jenis_param = "K" THEN -- jika keluar
-				-- update saldo, saldo = (saldo-nominal lama)+nominal baru
-				UPDATE m_saldo SET saldo = ((saldo_param-nominal_lama)+nominal_param);
-			ELSE -- jika masuk
-				-- update saldo, saldo = (saldo+nominal lama)-nominal baru
-				UPDATE m_saldo SET saldo = ((saldo_param+nominal_lama)-nominal_param);
-			END IF;
-		END IF;
-	END IF;
-END;
-
--- =============================================================
-
--- procedure hapus keuangan
--- hapus data jika memang data tidak pernah ada
--- update saldo
-CREATE PROCEDURE hapus_keuangan(
-	in id_param int
-)
-BEGIN
-	DECLARE saldo_param double(12,2);
-	DECLARE nominal_lama double(10,2);
-
-	-- get saldo terakhir
-	SELECT saldo INTO saldo_param FROM m_saldo;
-
-	-- get data nominal
-	SELECT nominal INTO nominal_lama FROM t_keuangan WHERE id = id_param;
-
-	-- update saldo
-	UPDATE m_saldo SET saldo = (saldo_param-nominal_lama);
-
-	-- hapus t_keuangan
-	DELETE FROM t_keuangan WHERE id = id_param;
 END;
 
 -- =============================================================
@@ -402,11 +249,12 @@ END;
 -- view member
 CREATE OR REPLACE VIEW v_member AS
 	SELECT m.id id_member, m.no_ktp, m.nama, m.no_telp, m.alamat, m.pekerjaan, 
-		GROUP_CONCAT(n.nopol separator ', ') nopol
-	FROM m_member m
-	JOIN m_nopol n
+		GROUP_CONCAT(n.nopol) nopol
+	FROM member m
+	JOIN nopol n
 		ON n.id_member = m.id
-	ORDER BY m.nama ASC
+	GROUP BY m.id
+	ORDER BY m.nama ASC;
 
 -- =============================================================
 
@@ -414,45 +262,53 @@ CREATE OR REPLACE VIEW v_member AS
 CREATE OR REPLACE VIEW v_admin AS
 	SELECT username, nama, email, hak_akses, level,
 		(CASE WHEN (status = "1") THEN "AKTIF" ELSE "NON AKTIF" END) AS status
-	FROM m_admin
-	ORDER BY level DESC, status ASC
+	FROM admin
+	ORDER BY level DESC, status ASC;
 
 -- =============================================================
 
 -- view log_admin
 CREATE OR REPLACE VIEW v_log_admin AS
 	SELECT log.id, log.admin, a.nama, log.login, log.ip
-	FROM t_log_admin log
-	JOIN m_admin a
+	FROM log_admin log
+	JOIN admin a
 		ON a.username = log.admin
-	ORDER BY log.id DESC
+	ORDER BY log.id DESC;
 -- =============================================================
 
 -- view transaksi sewa
 CREATE OR REPLACE VIEW v_sewa AS
-	SELECT s.id id_sewa, m.nama, p.no_parkir, 
+	SELECT s.id id_sewa, s.id_member, m.nama, s.id_nopol, n.nopol, p.no_parkir, 
 		(CASE WHEN (s.jenis = "B") THEN "BULANAN" ELSE "TAHUNAN" END) jenis,
 		s.harga, s.tgl_sewa, s.jatuh_tempo,
 		(CASE WHEN (s.status = "1") THEN "MASIH BERLAKU" ELSE "KONTRAK HABIS" END) status,
 		s.admin, a.nama nama_admin
-	FROM t_sewa s
-	JOIN v_member m
-		ON m.id_member = s.id_member
-	JOIN m_parkir p
+	FROM sewa s
+	JOIN member m
+		ON m.id = s.id_member
+	JOIN nopol n
+		ON n.id = s.id_nopol
+	JOIN parkir p
 		ON p.id = s.id_parkir
-	JOIN m_admin a
+	JOIN admin a
 		ON a.username = s.admin
-	ORDER BY s.id DESC, status DESC, s.tgl_sewa DESC
+	ORDER BY s.id DESC, status DESC, s.tgl_sewa DESC;
 -- =============================================================
 
 -- view keuangan
 CREATE OR REPLACE VIEW v_keuangan AS
 	SELECT k.id, k.tgl,
 		(CASE WHEN (k.jenis = "K") THEN "UANG KELUAR" ELSE "UANG MASUK" END) jenis,
-		k.ket, k.admin, a.nama nama_admin
-	FROM t_keuangan k
-	JOIN m_admin a
+		k.ket, k.nominal, k.admin, a.nama nama_admin
+	FROM keuangan k
+	JOIN admin a
 		ON a.username = k.admin
-	ORDER BY k.id DESC
+	ORDER BY k.id DESC;
 
 -- =============================================================
+
+CREATE OR REPLACE VIEW v_saldo AS
+	SELECT
+		(SELECT COALESCE(SUM(harga),0) FROM sewa) uang_sewa,
+	    (SELECT COALESCE(SUM(nominal),0) FROM keuangan WHERE jenis='K') uang_keluar,
+		(SELECT COALESCE(SUM(nominal),0) FROM keuangan WHERE jenis='M') uang_masuk;
---
+++ app/db/uji_coba.sql
@@ -0,0 +1,115 @@
+-- parkir
+INSERT INTO parkir(no_parkir, status) 
+VALUES 
+('A01','1'), ('A02','1'), ('A03','1'), ('A04','1'), ('A05','1'), ('A06','1'), ('A07','1'), ('A08','1'), ('A09','1'),
+('B01','1'), ('B02','1'), ('B03','1'), ('B04','1'), ('B05','1'), ('B06','1'), ('B07','1'), ('B08','1'), ('B09','1');
+
+-- harga
+INSERT INTO harga(jenis, harga)
+VALUES ('B', 250000), ('T', 25000000);
+
+-- member
+INSERT INTO member(no_ktp, nama, no_telp, alamat, pekerjaan)
+VALUES
+('3273245106840001', 'TIA PURNAMASARI', '089508014907', 'JL. CISARANTEN KULON NO. 100 RT 04/03 KEC. ARCAMANIK BANDUNG', 'IBU RUMAH TANGGA'),
+('3204250312830012', 'KRIS SYANDI KURNIA', '081394461234', 'JL. CISARANTEN KULON 8E B', 'WIRASWASTA'),
+('', 'SAUDARA MBAK TIA', '', '', ''),
+('', 'AYI TOHIR', '081909996908', 'JL. CISARANTEN KULON RT 03/03', 'SWASTA'),
+('3523172006850002', 'WAWAN FITRI ANTO', '085854004722', '&QUOT;MULTI USAHA GYPSUM&QUOT; CISARANTEN KULON NO. 7AB (DEPAN KANTOR POS/GIRO) ARCAMANIK', 'SWASTA'),
+('3273242003760002', 'DIRMAN SUDIRMAN', '085353471597', 'KP. CISARANTEN KULON RT 01/03 ARCAMANIK', 'WIRASWASTA'),
+('', 'LIA', '', '', ''),
+('', 'HENHEN', '', '', ''),
+('', 'YAYAT', '', '', '');
+
+-- nopol
+INSERT INTO nopol(id_member, nopol)
+VALUES
+(1, 'D 1147 OC') -- tia
+(2, 'D 1073 1B') -- kris
+(2, 'D 1091 QR') -- kris
+(3, '') -- saudara mbak tia
+(4, '') -- ayi
+(5, 'W 1549 BV') -- wawan
+(6, 'D 1315 AEQ') -- dirman
+(7, '') -- lia
+(8, '') -- henhen
+(9, '') -- yayat
+
+-- sewa
+-- CALL tambah_sewa(id_member, id_parkir, id_nopol, jenis, harga, tgl, jatuh, status, admin);
+
+	-- bulan april
+	CALL tambah_sewa(1, 1, 1, 'B', 250000, '2017-04-06', '2017-05-06', '0', 'lordRaze'); -- tia
+	CALL tambah_sewa(2, 2, 2, 'B', 250000, '2017-04-08', '2017-05-08', '0', 'lordRaze'); -- kris
+	CALL tambah_sewa(3, 3, 4, 'B', 250000, '2017-04-20', '2017-05-20', '0', 'lordRaze'); -- saudara mbak lia
+	CALL tambah_sewa(4, 4, 5, 'B', 250000, '2017-04-22', '2017-05-22', '0', 'lordRaze'); -- ayi tohir
+	CALL tambah_sewa(5, 5, 6, 'B', 250000, '2017-04-23', '2017-05-23', '0', 'lordRaze'); -- wawan
+	CALL tambah_sewa(6, 6, 7, 'B', 250000, '2017-04-27', '2017-05-27', '0', 'lordRaze'); -- dirman
+
+	-- bulan mei
+	CALL tambah_sewa(1, 1, 1, 'B', 250000, '2017-05-06', '2017-06-06', '0', 'lordRaze'); -- tia
+	CALL tambah_sewa(2, 2, 2, 'B', 250000, '2017-05-08', '2017-06-08', '0', 'lordRaze'); -- kris
+	CALL tambah_sewa(7, 3, 8, 'B', 250000, '2017-05-10', '2017-06-10', '0', 'lordRaze'); -- lia
+	CALL tambah_sewa(4, 4, 5, 'B', 250000, '2017-05-22', '2017-06-22', '0', 'lordRaze'); -- ayi
+	CALL tambah_sewa(5, 5, 6, 'B', 250000, '2017-05-23', '2017-06-23', '0', 'lordRaze'); -- wawan
+	CALL tambah_sewa(6, 6, 7, 'T', 2500000, '2017-05-30', '2018-05-30', '0', 'lordRaze'); -- dirman
+	CALL tambah_sewa(2, 7, 2, 'B', 250000, '2017-05-31', '2017-07-01', '0', 'lordRaze'); -- kris
+
+	-- bulan juni
+	CALL tambah_sewa(1, 1, 1, 'B', 250000, '2017-06-06', '2017-07-06', '0', 'lordRaze'); -- tia
+	CALL tambah_sewa(2, 2, 2, 'B', 250000, '2017-06-08', '2017-07-08', '0', 'lordRaze'); -- kris
+
+	-- bulan juli
+	CALL tambah_sewa(4, 4, 5, 'B', 250000, '2017-07-04', '2017-08-04', '0', 'lordRaze'); -- ayi
+	CALL tambah_sewa(1, 1, 1, 'B', 250000, '2017-07-06', '2017-08-06', '0', 'lordRaze'); -- tia
+	CALL tambah_sewa(2, 2, 2, 'B', 250000, '2017-07-08', '2017-08-08', '0', 'lordRaze'); -- kris
+	CALL tambah_sewa(5, 5, 6, 'B', 250000, '2017-07-12', '2017-08-12', '0', 'lordRaze'); -- wawan
+	CALL tambah_sewa(8, 3, 9, 'B', 250000, '2017-07-14', '2017-08-14', '0', 'lordRaze'); -- henhen
+	CALL tambah_sewa(2, 7, 2, 'B', 250000, '2017-07-27', '2017-08-27', '0', 'lordRaze'); -- kris
+
+	-- bulan agustus
+	CALL tambah_sewa(4, 4, 5, 'B', 250000, '2017-08-04', '2017-09-04', '0', 'lordRaze'); -- ayi
+	CALL tambah_sewa(1, 1, 1, 'B', 250000, '2017-08-06', '2017-09-06', '0', 'lordRaze'); -- tia
+	CALL tambah_sewa(2, 2, 2, 'B', 250000, '2017-08-08', '2017-09-08', '0', 'lordRaze'); -- kris
+	CALL tambah_sewa(5, 5, 6, 'B', 250000, '2017-08-12', '2017-09-12', '0', 'lordRaze'); -- wawan
+	CALL tambah_sewa(8, 3, 9, 'B', 250000, '2017-08-14', '2017-09-14', '0', 'lordRaze'); -- henhen
+	CALL tambah_sewa(2, 7, 2, 'B', 250000, '2017-08-27', '2017-09-27', '0', 'lordRaze'); -- kris
+
+	-- bulan september
+	CALL tambah_sewa(9, 8, 10, 'B', 250000, '2017-09-01', '2017-10-01', '0', 'lordRaze'); -- yayat
+	CALL tambah_sewa(4, 4, 5, 'B', 250000, '2017-09-04', '2017-10-04', '0', 'lordRaze'); -- ayi
+	CALL tambah_sewa(1, 1, 1, 'B', 250000, '2017-09-06', '2017-10-06', '0', 'lordRaze'); -- tia
+	CALL tambah_sewa(2, 2, 2, 'B', 250000, '2017-09-08', '2017-10-08', '0', 'lordRaze'); -- kris
+	CALL tambah_sewa(5, 5, 6, 'B', 250000, '2017-09-12', '2017-10-12', '0', 'lordRaze'); -- wawan
+	CALL tambah_sewa(8, 3, 9, 'B', 250000, '2017-09-14', '2017-10-14', '0', 'lordRaze'); -- henhen
+	CALL tambah_sewa(2, 7, 2, 'B', 250000, '2017-09-27', '2017-10-27', '0', 'lordRaze'); -- kris
+
+	-- bulan oktober
+	CALL tambah_sewa(9, 8, 10, 'B', 250000, '2017-10-01', '2017-11-06', '1', 'lordRaze'); -- yayat
+	CALL tambah_sewa(4, 4, 5, 'B', 250000, '2017-10-04', '2017-11-04', '1', 'lordRaze'); -- ayi
+	CALL tambah_sewa(2, 2, 2, 'B', 250000, '2017-10-08', '2017-11-08', '1', 'lordRaze'); -- kris
+	CALL tambah_sewa(2, 7, 2, 'B', 250000, '2017-10-27', '2017-11-27', '1', 'lordRaze'); -- kris
+
+-- keuangan
+INSERT INTO keuangan(tgl, jenis, ket, nominal, admin) 
+VALUES
+('2017-04-25', 'k', 'DUPLIKAT KUNCI 7 BUAH @12.000/KUNCI', 85000,'lordRaze'),
+('2017-04-25', 'k', 'PILOK WARNA MERAH UNTUK LABEL PARKIR @20.500/BUAH', 65000,'lordRaze'),
+('2017-05-10', 'k', 'PERLENGKAPAN LAMPU UNTUK GARASI: KABEL 30M @150.000, 1 SET LAMPU NEON @45.000, 1 SET LAMPU LED BESERTA PERLENGKAPAN LAINNYA @34.000', 250000,'lordRaze'),
+('2017-06-08', 'k', 'LISTRIK GARASI', 75000,'lordRaze'),
+('2017-06-18', 'k', 'KEPERLUAN LEBARAN', 2000000,'lordRaze'),
+('', 'k', 'TAKSI KE KOTABUMI', 350000,'lordRaze'),
+('2017-07-05', 'k', 'BELI LAMPU NEON SATU SET, KABEL 10M, BESERTA UANG JASA PASANG, DLL.', 200000,'lordRaze'),
+('2017-07-10', 'k', 'LISTRIK GARASI', 75000,'lordRaze'),
+('2017-07-13', 'k', 'UANG JASA PANGGIL ORANG CEK POMPA AIR', 100000,'lordRaze'),
+('2017-07-14', 'k', 'UANG BENSIN MOBIL YARIS ANUN', 75000,'lordRaze'),
+('2017-07-17', 'k', 'INTERNET BANDUNG', 540000,'lordRaze'),
+('2017-08-10', 'k', 'LISTRIK GARASI', 75000,'lordRaze'),
+('2017-08-11', 'k', 'GANTI ACCU MOTOR MIO', 250000,'lordRaze'),
+('2017-08-11', 'k', 'GANTI LAMPU RUMAH', 50000,'lordRaze'),
+('2017-08-16', 'k', 'INTERNET BANDUNG', 550000,'lordRaze'),
+('2017-09-08', 'k', 'INTERNET BANDUNG', 550000,'lordRaze'),
+('2017-09-10', 'k', 'LISTRIK GARASI', 75000,'lordRaze'),
+('2017-10-09', 'k', 'LISTRIK GARASI', 75000,'lordRaze'),
+('2017-10-09', 'k', 'IURAN RW 2 BULAN', 75000,'lordRaze'),
+('2017-10-09', 'k', 'INTERNET BANDUNG', 540000,'lordRaze');
